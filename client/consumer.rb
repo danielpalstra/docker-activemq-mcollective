01344deb89c14d4365778b940c85fd23bc91d67f
Daniel Palstra <daniel@danielpalstra.com>
2017-08-08 14:41:27 +0200

    Working setup

---
+++ client/consumer.rb
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+
+require 'rubygems'
+require 'stomp'
+#
+# == Example message consumer.
+#
+class ExampleConsumer
+  # Initialize.
+  def initialize; end
+
+  # Run example.
+  def run
+    # client = Stomp::Client.new('failover://(stomp://:@localhost:61613,stomp://:@remotehost:61613)?initialReconnectDelay=5000&randomize=false&useExponentialBackOff=false')
+
+    client = Stomp::Client.new('stomp://:@localhost:61613')
+
+    puts 'Subscribing ronaldo'
+    client.subscribe('/queue/ronaldo', :ack => 'client',
+     'activemq.prefetchSize' => 1,
+     'activemq.exclusive' => true) do |msg|
+      File.open('file', 'a') do |f|
+        puts(msg.body)
+        puts("\n----------------\n")
+      end
+
+      client.acknowledge(msg)
+    end
+
+    loop do
+      sleep(5)
+      puts '.'
+    end
+  end
+end
+#
+e = ExampleConsumer.new
+e.run
